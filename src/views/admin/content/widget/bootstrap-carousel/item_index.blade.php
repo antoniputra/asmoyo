c5dca580881f7d27f5c572bf8b9e52b9f435a475
antoniputra <akiddcode@gmail.com>
2014-09-14 23:11:37 +0700

    update no result view

--- src/views/admin/content/widget/bootstrap-carousel/item_index.blade.php
+++ src/views/admin/content/widget/bootstrap-carousel/item_index.blade.php
@@ -20,45 +20,45 @@
 		</a>
 	</h3>
 	<div class="box-content">
-		<table class="table table-bordered table-hover">
-			<thead>
-				<tr>
-					<th style="width:30%;">Image</th>
-					<th style="width:40%;">Description</th>
-					<th>Action</th>
-				</tr>
-			</thead>
-			<tbody>
-				@if( count($items) )
-				@foreach($items as $key => $item)
+		@if( count($items) )
+			<table class="table table-bordered table-hover">
+				<thead>
 					<tr>
-						<td>
-							<div class="thumbnail">
-								<img src="{{ $item['image'] }}">
-							</div>
-						</td>
-						<td>
-							<h4>{{$item['title']}}</h4>
-							<p>{{$item['description']}}</p>
-						</td>
-						<td style="vertical-align:middle;">
-							<a href="{{ admin_route('widget.item.edit', [$widget['name'], $cat['slug'], $item['id']]) }}" class="btn btn-default btn-sm">
-								<i class="fa fa-pencil"></i> Edit
-							</a>
-							{{ Form::link('Hapus Permanent', 'DELETE', admin_route('widget.item.forceDestroy', array($widget['name'], $cat['slug'], $item['id'])),
-								array(
-									'icon'	=> 'fa fa-trash-o',
-									'class'	=> 'btn btn-danger btn-sm'
-								),
-								'Apakah anda yakin ?'
-							) }}
-						</td>
+						<th style="width:30%;">Image</th>
+						<th style="width:40%;">Description</th>
+						<th>Action</th>
 					</tr>
-				@endforeach
-				@else
-					<h4>Tidak ada item</h4>
-				@endif
-			</tbody>
-		</table>
+				</thead>
+				<tbody>
+					@foreach($items as $key => $item)
+						<tr>
+							<td>
+								<div class="thumbnail">
+									<img src="{{ $item['image'] }}">
+								</div>
+							</td>
+							<td>
+								<h4>{{$item['title']}}</h4>
+								<p>{{$item['description']}}</p>
+							</td>
+							<td style="vertical-align:middle;">
+								<a href="{{ admin_route('widget.item.edit', [$widget['name'], $cat['slug'], $item['id']]) }}" class="btn btn-default btn-sm">
+									<i class="fa fa-pencil"></i> Edit
+								</a>
+								{{ Form::link('Hapus Permanent', 'DELETE', admin_route('widget.item.forceDestroy', array($widget['name'], $cat['slug'], $item['id'])),
+									array(
+										'icon'	=> 'fa fa-trash-o',
+										'class'	=> 'btn btn-danger btn-sm'
+									),
+									'Apakah anda yakin ?'
+								) }}
+							</td>
+						</tr>
+					@endforeach
+				</tbody>
+			</table>
+		@else
+			<h4>Tidak ada item</h4>
+		@endif
 	</div>
 </div>
